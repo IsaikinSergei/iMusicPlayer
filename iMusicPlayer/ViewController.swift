b83a8e09174ec4837a6238fa3483c3bb18651563
IsaikinSergei <akinfest007@gmail.com>
2021-04-25 23:31:37 +0400

    Initial Commit

---
+++ iMusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMusicPlayer
+//
+//  Created by Sergei Isaikin on 25.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

b87eeb3304b1905b60cb9ff6a22a1c9d593bdb16
IsaikinSergei <akinfest007@gmail.com>
2021-04-26 01:04:32 +0400

    Added MainTabBarController

---
+++ iMusicPlayer/MainTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  MainTabBarController.swift
+//  iMusicPlayer
+//
+//  Created by Sergei Isaikin on 25.04.2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
+        
+        viewControllers = [ generateViewController(rootViewController: SearchViewController(), image: #imageLiteral(resourceName: "search"), title: "Search"), generateViewController(rootViewController: ViewController(), image: #imageLiteral(resourceName: "library"), title: "Library") ]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        return navigationVC
+    }
+    
+}
---
+++ iMusicPlayer/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  iMusicPlayer
+//
+//  Created by Sergei Isaikin on 26.04.2021.
+//
+
+import UIKit
+
+class SearchViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}

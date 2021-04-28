5f3f0e2767903eb5954c78f639e7745f7446910a
IsaikinSergei <akinfest007@gmail.com>
2021-04-29 00:36:12 +0400

    Added using methods template CleanSwift

--- iMusicPlayer/MainTabBarController.swift
+++ iMusicPlayer/MainTabBarController.swift
@@ -16,7 +16,9 @@ class MainTabBarController: UITabBarController {
         
         tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
         
-        viewControllers = [ generateViewController(rootViewController: SearchMusicViewController(), image: #imageLiteral(resourceName: "search"), title: "Search"), generateViewController(rootViewController: ViewController(), image: #imageLiteral(resourceName: "library"), title: "Library") ]
+        let searchVC: SearchViewController = SearchViewController.loadFromStoryboard()
+        
+        viewControllers = [ generateViewController(rootViewController: searchVC, image: #imageLiteral(resourceName: "search"), title: "Search"), generateViewController(rootViewController: ViewController(), image: #imageLiteral(resourceName: "library"), title: "Library") ]
     }
     
     private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController {

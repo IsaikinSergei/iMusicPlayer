1c24b46d50254575831fd8c71d441080ac7d9753
IsaikinSergei <akinfest007@gmail.com>
2021-05-01 00:23:03 +0400

    Added Creating Track Cells by XIB

---
+++ iMusicPlayer/Search/Cell/TrackCell.swift
@@ -0,0 +1,36 @@
+//
+//  TrackCell.swift
+//  iMusicPlayer
+//
+//  Created by Sergei Isaikin on 30.04.2021.
+//
+
+import UIKit
+
+protocol TrackCellViewModel {
+    var iconUrlString: String? { get }
+    var trackName: String { get }
+    var artistName: String { get }
+    var collectionName: String { get }
+}
+
+class TrackCell: UITableViewCell {
+    
+    static let reuseId = "TrackCell"
+    
+    @IBOutlet weak var trackNameLabel: UILabel!
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var collectionNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func set(viewModel: TrackCellViewModel) {
+        trackNameLabel.text = viewModel.trackName
+        artistNameLabel.text = viewModel.artistName
+        collectionNameLabel.text = viewModel.collectionName
+    }
+    
+}

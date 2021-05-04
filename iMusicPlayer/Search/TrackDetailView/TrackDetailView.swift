7deb73ae332738082016c1e23c6c2cdf8829e93d
IsaikinSergei <akinfest007@gmail.com>
2021-05-04 16:12:35 +0400

    Added design of screen (outlets and actions)

--- iMusicPlayer/Search/TrackDetailView/TrackDetailView.swift
+++ iMusicPlayer/Search/TrackDetailView/TrackDetailView.swift
@@ -9,8 +9,33 @@ import UIKit
 
 class TrackDetailView: UIView {
     
-    override class func awakeFromNib() {
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var trackTitleLabel: UILabel!
+    @IBOutlet weak var authorTitleLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    override func awakeFromNib() {
         super.awakeFromNib()
+        
+        trackImageView.backgroundColor = .red
+    }
+    
+    @IBAction func handleCurrentTimerSlider(_ sender: Any) {
+    }
+    @IBAction func handleVolumeSlider(_ sender: Any) {
+    }
+    @IBAction func dragDownButtonTapped(_ sender: Any) {
+        self.removeFromSuperview()
+    }
+    @IBAction func previousTrack(_ sender: Any) {
+    }
+    @IBAction func nextTrack(_ sender: Any) {
+    }
+    @IBAction func playPauseAction(_ sender: Any) {
     }
     
 }
